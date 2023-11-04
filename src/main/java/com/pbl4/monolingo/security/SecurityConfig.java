604f75c173f2c9c617b9a8e9d8d6f0197b684647
lekhoa178 <leanhkhoapl178@gmail.com>
2023-11-04 17:31:20 +0700

    Add http basic to spring security

--- src/main/java/com/pbl4/monolingo/security/SecurityConfig.java
+++ src/main/java/com/pbl4/monolingo/security/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
 
 
         // use HTTP Basic authentication
+        http.httpBasic();
         http.formLogin();
 //        http.formLogin(formLogin -> formLogin
 //                /*.loginPage("/login")*/.permitAll());

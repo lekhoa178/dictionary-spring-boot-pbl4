976bb5701250af1f7ba58a6ee2151f47afea6bf7
lekhoa178 <leanhkhoapl178@gmail.com>
2023-11-16 18:13:09 +0700

    game rest controller

---
+++ src/main/java/com/pbl4/monolingo/rest/GameRestController.java
@@ -0,0 +1,9 @@
+package com.pbl4.monolingo.rest;
+
+import com.pbl4.monolingo.dao.StageRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameRestController {
+
+}

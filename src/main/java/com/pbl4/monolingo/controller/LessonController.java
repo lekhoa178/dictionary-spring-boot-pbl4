fa973a2a09c30f97508eb7bef44fd5f091d1e364
Leon <ducnghianguyen36@gmail.com>
2023-11-25 14:43:59 +0700

    update game

--- src/main/java/com/pbl4/monolingo/controller/LessonController.java
+++ src/main/java/com/pbl4/monolingo/controller/LessonController.java
@@ -1,17 +1,28 @@
 package com.pbl4.monolingo.controller;
 
-import com.pbl4.monolingo.entity.embeddable.LevelId;
+import com.pbl4.monolingo.entity.Account;
+import com.pbl4.monolingo.entity.ExtraInformation;
 import com.pbl4.monolingo.service.AccountService;
+import com.pbl4.monolingo.service.ExtraInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/lesson")
 public class LessonController {
 
-
+    private final AccountService accountService;
+    private final ExtraInfoService extraInfoService;
+    @Autowired
+    public LessonController(ExtraInfoService extraInfoService, AccountService accountService) {
+        this.accountService = accountService;
+        this.extraInfoService = extraInfoService;
+    }
 
     @GetMapping("/{stageId}/{levelId}")
 
@@ -39,4 +50,16 @@ public class LessonController {
         return "lessonFinish.html";
     }
 
+    @PostMapping("/lostHeart")
+    public ResponseEntity<Integer> lostHeart(Principal principal) {
+        Account account = accountService.getAccountByUsername(principal.getName());
+        extraInfoService.loseHeart(account.getAccountId());
+        System.out.println(account.getExtraInformation().getHearts());
+        return ResponseEntity.ok(account.getExtraInformation().getHearts());
+    }
+
+    @GetMapping("/heart")
+    public ResponseEntity<Integer> getHeart(Principal principal) {
+        return ResponseEntity.ok(accountService.getAccountInfoByUsername(principal.getName()).getHearts());
+    }
 }
